fd8582c85bac1898367065bcd6b65c9db5b3f0e0
Cabnit <colek.206@gmail.com>
2024-05-10 14:25:19 -0700

    added a working searchbar and button and made the css work also made it show you the names of the recipes for the searched ingrediants.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  // Function to make the request and update the result div
  function makeRequest() {
-    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=cheese')
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=' + encodeURIComponent(document.getElementById('ingredients-search').value))
         .then(response => {
             //the ! means that if the response is NOT true then it will throw a new error which catch will tell us
             if (!response.ok) {
@@ -14,15 +14,24 @@
         })
         .then(data => {
             // Update the result div with the data
-            document.getElementById('result').textContent = JSON.stringify(data);
+            for(i = 0; i < data.meals.length; i++){
+                console.log("test")
+                document.getElementById('result').innerHTML += `
+                <h1> ${data.meals[i]["strMeal"]} </h1>
+                <img src="${data.meals[i]["strMealThumb"]}">
+                `
+            }
+            
         })
         // this will make it so if there is an error it will let us know
         .catch(error => console.error (error))
         };
 
-        function kms(){
-            console.log("Daniel is a fatass")
-        }
+function kms(){
+    makeRequest()
+    loadRecepie(5496)
+}
 
-// Call the function when the page loads
-window.addEventListener('load', makeRequest);
+function loadRecepie(recepieId){
+    console.log(recepieId)
+}
